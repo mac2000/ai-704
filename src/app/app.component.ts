ba7d369cb29cc94713a307c8657353f179871214
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2020-12-28 17:55:02 +0200

    On a home component add code to query and display results from any API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ai704';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(todo => this.title = todo['title'])
+  }
 }
